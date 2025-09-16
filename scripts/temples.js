fd67486c9095749b21266e1d9f183ba971a6e82f
banzaseraphin <seraphimkiongo@.com>
2025-09-16 03:07:42 +0200

    changes

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,1 +1,14 @@
 document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
+
+
+
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("nav-menu");
+
+hamburger.addEventListener("click", () => {
+  navMenu.classList.toggle("open");
+
+  // Change button symbol
+  hamburger.textContent = navMenu.classList.contains("open") ? "X" : "☰";
+});
+
